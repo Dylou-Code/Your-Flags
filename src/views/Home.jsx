f6a9aafafdd2e28296b69229d3cd2880000f45fd
Dylou-Code <ceprik.dydy@gmail.com>
2023-08-07 00:45:04 +0200

    Feat: grid desktop, padding & margin card

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from "../components/Navigation.jsx";
 import Countries from "../components/Countries.jsx";
+import Footer from "../components/Footer.jsx";
 
 const Home = () => {
     return (
        <div className="container">
            <Navigation/>
            <Countries />
+           <Footer />
        </div>
     );
 };
